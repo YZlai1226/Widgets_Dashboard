e8016f4efd59601e4e053a90433491e5a6166bff
Nicolas Bretécher <bretechern@gmail.com>
2022-05-04 19:28:30 +0200

    start ingredients list

--- back2basics-front/src/components/widgetsComponents/Widget1.js
+++ back2basics-front/src/components/widgetsComponents/Widget1.js
@@ -2,11 +2,20 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function CocktailComponent(props) {
-useEffect(() => {
-  console.log(props)
-}, [props])
-return <>
+  useEffect(() => {
+    console.log(props)
+  }, [props])
+  /* const ingredients = props.cocktail.slice(18, 33);
+  console.log(ingredients); */
+  return <>
     <h1>{props.cocktail.strDrink}</h1>
+    {/* <p>Ingredients:</p>
+    <ul>
+    for 
+      if (props.cocktail.strIngredient1){
+        <li>{props.cocktail.strMeasure1}{props.cocktail.strIngredient1}</li>
+      }
+    </ul> */}
     <h3>Glass: {props.cocktail.strGlass}</h3>
     <p>{props.cocktail.strInstructions}</p>
   </>
@@ -18,14 +27,14 @@ export default function CocktailContainer() {
 
   async function aBoire() {
     console.log('Fetching a drink');
-    const res = await axios('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+    const res = await axios('https://cocktails-back.herokuapp.com/api/drinks');
+    //const res = await axios('https://www.thecocktaildb.com/api/json/v1/1/random.php');
     setCocktail(res.data.drinks[0]);
     setLoaded(true);
   }
 
   return (
     <div class="bigWidget">
-      <button onClick={aBoire}>Fetch me a cocktail!</button>
       {
         loaded === true ?
           <CocktailComponent
@@ -34,6 +43,8 @@ export default function CocktailContainer() {
           :
           <p>Not yet loaded</p>
       }
+      <button onClick={aBoire}>Fetch me another one!</button>
+
     </div>
   );
 }
